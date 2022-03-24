c4f82be54657d36b9210ad918518c4bbbe6e4024
adzhumaliev7 <adzhumaliev7@gmail.com>
2022-03-24 18:00:55 +0600

    COmmit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "73ece32645e9409fb4982a5bfa4381d1"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
